64e045085f9e6d67c8d78d247ac3746760b393a6
Anshuman_Moharana <iamanshuman7777@gmail.com>
2024-02-04 15:17:05 +0530

    day_3 added

---
+++ day_3/child_process.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process");
+
+function executeCommand(command) {
+    exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+}
+executeCommand('dir ');
+executeCommand('echo "Hello, Node.js!"');
