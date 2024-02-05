78ab3cc0ae5f1b77984ed6b4783602802bbcf588
Anshuman_Moharana <iamanshuman7777@gmail.com>
2024-02-05 20:23:23 +0530

    day-5 added

---
+++ day_5/file_extension.js
@@ -0,0 +1,16 @@
+const path=require('path')
+
+function checkFileExtension(filePath, expectedExtension) {
+    const expected=path.extname(filePath);
+    if(expected===expectedExtension)
+    {
+        console.log(`File has the expected extension: ${expectedExtension}`);
+    }
+    else{
+        console.log(`File does not have the expected extension. Expected: ${expectedExtension}, Actual: ${expected}`);
+    }
+}
+
+//TESTCASES
+checkFileExtension('test-files/file1.txt', '.txt');
+checkFileExtension('test-files/image.png', '.jpg');
